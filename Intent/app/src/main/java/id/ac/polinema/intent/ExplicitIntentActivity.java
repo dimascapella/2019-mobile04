292bd57541fcde1e899c1ddd5fe3f818aece2e3a
dimas eka adinandra <dimascapella1590@gmail.com>
2019-09-20 20:38:47 +0700

    Intent Jobsheet

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -6,13 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+
+    private EditText ambil;
+    private TextView tampil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        ambil = findViewById(R.id.input_name);
+        tampil = findViewById(R.id.text_output);
     }
 
+    public void submit(View view) {
+        String text = ambil.getText().toString();
+        if(text.equals("")){
+            Toast.makeText(this, "Fill Form, Please", Toast.LENGTH_SHORT).show();
+        }else{
+            tampil.setText("Hello " + text + " ,Congratulation!");
+        }
+    }
 }
